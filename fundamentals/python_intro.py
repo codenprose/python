3234c6b2bc8cd7adcf312c17fcbf287ffe964ba2
dkh215 <danielkhunter@gmail.com>
2017-01-02 15:02:53 -0500

    Initial Commit

---
+++ fundamentals/python_intro.py
@@ -0,0 +1,30 @@
+# Hello World
+print("Hello World")
+
+# Error Types
+# -----------
+# Name Error = You try to use a variable that doesn't exist
+print(hello_world)
+
+# Type Error = Trying to add an integer to a string e.g. 2 + "Two"
+print("Hello World" + 5)
+
+# Syntax Error = You've written some code incorrectly and Python can't understand it
+print("This print function isn't closed"
+
+# Variables and Data Types
+name = "Daniel K. Hunter" # String
+age = 10 * 3 - 1 # Number
+dkh = { # Dictonary
+    'name': name,
+    'age': age
+}
+my_list = [name, age, dkh] # List
+
+# Numbers
+# Division always returns a float
+8 / 2 # = 4.0
+
+# Strings
+"Status Message: you currently have {} vistors on {}".format(30, "danielkhunter.com")
+"=" * 35 # ==================================
